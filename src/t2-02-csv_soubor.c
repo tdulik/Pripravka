f7c333d24ccb63b8f60eb891cd53badeed1ac2e9
tdulik <37401035+tdulik@users.noreply.github.com>
2024-08-27 13:53:26 +0200

    CSV soubor - nacitani souboru z disku

--- src/t2-02-csv_soubor.c
+++ src/t2-02-csv_soubor.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int cleanBuffer() {
+int cleanBuffer(FILE *f) {
     char znak;
     int pocet=0;
-    while ((znak = getchar()) != '\n') {
+    while ((znak = fgetc(f)) != '\n') {
         pocet++;
     };
     return pocet;
-}
-
-int vlozeniCisel(int pole[]) {
-    int pocet=0, i=0;
-    while (1) {
-        int status;
-        while ((status = scanf("%d", &pole[i])) != 1){
-          pocet=cleanBuffer();
-          if (pocet==1) return i;
-          printf("Promin ale error");
-        }
-      i++;
-
-    }
 }
 
 
@@ -30,13 +16,18 @@ int main() {
     int b;
     int c;
     double souceta = 0, soucetb = 0, soucetc = 0, pocet = 0;
-    char   jmeno[5];
+    char   jmeno[500];
+    FILE * f = fopen("znamky.txt", "r");
+    if (f == NULL) {
+        puts("Error opening file, I will use standard input...");
+        f=stdin;
+    }
     printf("Zadej jmeno a tri cisla: ");
     while(1) {
-        int status = scanf("%4[^,],%d,%d,%d", jmeno,&a,&b,&c);
+        int status = fscanf(f,"%499[^,],%d,%d,%d", jmeno,&a,&b,&c);
         if(status == 4) {
             printf("jmeno = %s, a = %d, b = %d, c = %d, status = %d\n", jmeno,a,b,c, status);
-            cleanBuffer(); //smazani prebytecneho \n na konci radku vstupu
+            cleanBuffer(f); //smazani prebytecneho \n na konci radku vstupu
             souceta +=a;
             soucetb +=b;
             soucetc +=c;
@@ -50,7 +41,7 @@ int main() {
                 break;
             }
             printf("Sorry mas to blbe, zkus to znovu.\n");
-            cleanBuffer();
+            cleanBuffer(f);
         }
     }
     printf("prumer a = %lf, prumer b = %lf, prumer c = %lf", souceta/pocet, soucetb/pocet, soucetc/pocet);
